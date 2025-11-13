a2ec8c5ccd19e1a361bfbdf9580035a9be1abb0e
Sistemas01 <sistemas01@Sistemas01--MacBook-Air.local>
2025-11-13 16:53:16 -0500

    AE-31: Update sector html

--- src/routes/(app)/area/[areaId]/sector/+page.svelte
+++ src/routes/(app)/area/[areaId]/sector/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import { type Status } from '$lib/contants/constants';
 
+	import logo from '$lib/assets/smallLogo.png';
+	import fullLogo from '$lib/assets/aeLogo.png';
+	import '$lib/styles/sector.css';
+
 	export let data: {
-		items: Array<{
+		items: {
 			id: string;
 			areaId: string;
 			name: string;
@@ -12,44 +16,111 @@
 			createdAt: string;
 			updatedAt?: string | null;
 			deletedAt?: string | null;
-		}>;
+		}[];
+		areaInfo: {
+			id: string;
+			name: string;
+			province: string;
+			city: string;
+			description: string;
+			latitude: number;
+			longitude: number;
+			status: Status;
+			createdAt: string;
+			updatedAt?: string | null;
+			deletedAt?: string | null;
+		}[];
 		page: number;
 		role: string;
 		status: string;
 	};
 
 	export let form: { message?: string; success?: boolean } | undefined;
+	let messageVisible = false;
+
+	let showForm = false;
+
+	function toggleForm() {
+		showForm = !showForm;
+	}
+	$: if (form?.message) {
+		messageVisible = true;
+		setTimeout(() => {
+			messageVisible = false;
+		}, 3000);
+	}
 </script>
 
-<h1 class="mb-4 text-xl">Sectores</h1>
+<section class="background">
+	<section class="header__container">
+		<header class="header">
+			<section class="header__logo-container">
+				<img class="logo" alt="logo" src={logo} />
+				<h1 class="header-text">Urqu Ascents</h1>
+			</section>
+			<nav class="menu">
+				<ul class="menu__list header-text">
+					<li><a class="menu__item" href="/dashboard">Menú Principal</a></li>
+					<li><a class="menu__item" href="/area">Áreas</a></li>
+					<!--Add Navigation to User Profile -->
+					<li><a class="menu__item" href="/area">Mi Perfil</a></li>
+				</ul>
+			</nav>
+		</header>
+	</section>
 
-{#if form?.message}
-	<p
-		class="mb-3 rounded border p-2 {form.success
-			? 'border-green-400 bg-green-50'
-			: 'border-red-400 bg-red-50'}"
-	>
-		{form.message}
-	</p>
-{/if}
+	<main>
+		<section class="main">
+			<section class="main__parent-description">
+				<h2 class="main__subtitle">{data.areaInfo[0].name}</h2>
+				<p class="main__area-info">{data.areaInfo[0].province}-{data.areaInfo[0].city}</p>
+			</section>
+			<section class="main__title-container">
+				<h1 class="main-title">Sectores</h1>
+				<button class="main__title-create" on:click={toggleForm}>
+					{showForm ? '×' : '+'}
+				</button>
+			</section>
+			<!-- Check-->
 
-<table class="w-full border-collapse">
-	<thead>
-		<tr>
-			<th class="border p-2 text-left">Nombre</th>
-			<th class="border p-2 text-left">Orientación</th>
-			<th class="border p-2">Descripción</th>
+			{#if form?.message && messageVisible}
+				<p
+					class="mb-3 rounded border p-2 {form.success
+						? 'border-green-400 bg-green-50'
+						: 'border-red-400 bg-red-50'}"
+				>
+					{form.message}
+				</p>
+			{/if}
+			<button formaction="">Filtros</button>
 
-			<th class="border p-2">Acciones</th>
-		</tr>
-	</thead>
-	<tbody>
-		{#each data.items as sector}
-			<tr>
-				<td class="border p-2">{sector.name}</td>
-				<td class="border p-2">{sector.orientation}</td>
-				<td class="border p-2">{sector.description}</td>
-				<td class="border p-2">
+			<!--TODO: ADD FILTERS-->
+
+			<table class="main__table">
+				<thead class="main__table-head">
+					<tr>
+						<th class="main__table-item">Nombre</th>
+						<th class="main__table-item">Orientación</th>
+						<th class="main__table-item">Descripción</th>
+						<!--TODO: Verify if this is neccessary
+						<th class="main__table-item">Acciones</th>
+						-->
+					</tr>
+				</thead>
+				<tbody class="main__table-tbody">
+					{#each data.items as sector}
+						<tr
+							class="main__table-body"
+							on:click={() =>
+								(window.location.href = `/area/${sector.areaId}/sector/${sector.id}/edit`)}
+						>
+							<td class="main__table-td">{sector.name}</td>
+							<td class="main__table-td">{sector.orientation}</td>
+							<td class="main__table-td">{sector.description}</td>
+							<td>→</td>
+							<!--TODO: Show only if admin
+
+				<td class="main__table-td">
 					<a href={`/area/${sector.areaId}/sector/${sector.id}/edit`}> Editar </a>
 					<form method="POST" class="inline">
 						<input type="hidden" name="id" value={sector.id} />
@@ -79,29 +150,48 @@
 						</form>
 					{/if}
 				</td>
-			</tr>
-		{/each}
-	</tbody>
-</table>
+				-->
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+			{#if showForm}
+				<form method="POST" enctype="multipart/form-data">
+					<h2>Crear nuevo sector</h2>
+					<br />
 
-<form method="POST" enctype="multipart/form-data">
-	<h2>Crear nuevo sector</h2>
-	<br />
+					<label for="name">Nombre:</label>
+					<input type="text" name="name" id="name" required />
 
-	<label for="name">Nombre:</label>
-	<input type="text" name="name" id="name" required />
+					<br />
 
-	<br />
+					<label for="orientation">Orientación:</label>
+					<input type="text" name="orientation" id="orientation" required />
 
-	<label for="orientation">Orientación:</label>
-	<input type="text" name="orientation" id="orientation" required />
+					<br />
 
-	<br />
+					<label for="description">Descripción:</label>
+					<input type="text" name="description" id="description" required />
 
-	<label for="description">Descripción:</label>
-	<input type="text" name="description" id="description" required />
+					<br />
 
-	<br />
+					<button type="submit" formaction="?/createSector">Crear Sector</button>
+				</form>
+			{/if}
+		</section>
+	</main>
+	<footer class="footer">
+		<img class="smallLogo" alt="logo" src={fullLogo} />
 
-	<button type="submit" formaction="?/createSector">Crear Sector</button>
-</form>
+		<section class="footer__links">
+			<a href="/dashboard">Inicio</a>
+			<a href="/area">Áreas</a>
+			<!--TODO: Add This Pages-->
+			<a href="/profile">Mi Perfil</a>
+			<a href="/contact">Contacto</a>
+		</section>
+		<p class="footer__text">
+			© {new Date().getFullYear()} Urqu Ascents
+		</p>
+	</footer>
+</section>
