c398e37df90443134bc716a90a52caf397fecf38
Sistemas01 <sistemas01@Sistemas01--MacBook-Air.local>
2025-11-23 10:30:40 -0500

    AE-45: Partial add users grades

--- src/routes/(app)/profile/[userId]/+page.server.ts
+++ src/routes/(app)/profile/[userId]/+page.server.ts
@@ -1,8 +1,8 @@
 import { db } from '$lib/server/db';
-import { users } from '$lib/server/db/schema';
+import { users, area, sector, climb, grade, climbingLevelSport } from '$lib/server/db/schema';
 import { requireUser } from '$lib/server/auth/guards';
 import type { PageServerLoad } from './$types';
-import { eq } from 'drizzle-orm';
+import { eq, and, inArray, desc, or } from 'drizzle-orm';
 
 export const load: PageServerLoad = async (event) => {
 	requireUser(event);
@@ -13,10 +13,171 @@ export const load: PageServerLoad = async (event) => {
 	const status = url.searchParams.get('status') ?? 'active';
 
 	const items = await db.select().from(users).where(eq(users.id, userId));
+	const userRopeClimbs = await db
+		.select({
+			areaId: area.id,
+			areaName: area.name,
+			sectorName: sector.name,
+			climbName: climb.name,
+			realValue: climb.value,
+			proposedValue: grade.value,
+			difficulty: grade.difficultyLevel,
+			done: grade.accomplished,
+			createdAt: grade.createdAt
+		})
+		.from(grade)
+		.leftJoin(climb, eq(climb.id, grade.climbId))
+		.leftJoin(sector, eq(sector.id, climb.sectorId))
+		.leftJoin(area, eq(area.id, sector.areaId))
+		.where(
+			and(
+				eq(grade.userId, userId),
+				inArray(grade.gradeSystem, ["Francesa", "YDS"])
+			)
+		)
+		.orderBy(desc(grade.createdAt));
+	const userNoRopeClimbs = await db
+		.select({
+			areaId: area.id,
+			areaName: area.name,
+			sectorName: sector.name,
+			climbName: climb.name,
+			realValue: climb.value,
+			proposedValue: grade.value,
+			difficulty: grade.difficultyLevel,
+			done: grade.accomplished,
+			createdAt: grade.createdAt
+		})
+		.from(grade)
+		.leftJoin(climb, eq(climb.id, grade.climbId))
+		.leftJoin(sector, eq(sector.id, climb.sectorId))
+		.leftJoin(area, eq(area.id, sector.areaId))
+		.where(
+			and(
+				eq(grade.userId, userId),
+				inArray(grade.gradeSystem, ["VScale", "Fontainebleau"])
+			)
+		)
+		.orderBy(desc(grade.createdAt));
+	const userTradClimbs = await db
+		.select({
+			areaId: area.id,
+			areaName: area.name,
+			sectorName: sector.name,
+			climbName: climb.name,
+			realValue: climb.value,
+			proposedValue: grade.value,
+			difficulty: grade.difficultyLevel,
+			done: grade.accomplished,
+			createdAt: grade.createdAt
+		})
+		.from(grade)
+		.leftJoin(climb, eq(climb.id, grade.climbId))
+		.leftJoin(sector, eq(sector.id, climb.sectorId))
+		.leftJoin(area, eq(area.id, sector.areaId))
+		.where(
+			and(
+				eq(grade.userId, userId),
+				eq(grade.gradeSystem, "British")
+			)
+		)
+		.orderBy(desc(grade.createdAt));
+	const topSport = await db
+		.select({
+			areaId: area.id,
+			areaName: area.name,
+			sectorName: sector.name,
+			climbName: climb.name,
+			gradeValue: grade.value,
+			scaledValue: climbingLevelSport.scaledValue,
+			createdAt: grade.createdAt
+		})
+		.from(grade)
+		.leftJoin(climb, eq(climb.id, grade.climbId))
+		.leftJoin(sector, eq(sector.id, climb.sectorId))
+		.leftJoin(area, eq(area.id, sector.areaId))
+		.leftJoin(
+			climbingLevelSport,
+			or(
+				eq(climbingLevelSport.frenchValue, grade.value),
+				eq(climbingLevelSport.ydsValue, grade.value)
+			)
+		)
+		.where(
+			and(
+				eq(grade.userId, userId),
+				eq(grade.accomplished, true),
+				inArray(grade.gradeSystem, ['Francesa', 'YDS'])
+			)
+		)
+		.orderBy(desc(climbingLevelSport.scaledValue))
+		.limit(3);
+		const topNoRope = await db
+  .select({
+    areaId: area.id,
+    areaName: area.name,
+    sectorName: sector.name,
+    climbName: climb.name,
+    gradeValue: grade.value,
+    scaledValue: climbingLevelNoRope.scaledValue,
+    createdAt: grade.createdAt
+  })
+  .from(grade)
+  .leftJoin(climb, eq(climb.id, grade.climbId))
+  .leftJoin(sector, eq(sector.id, climb.sectorId))
+  .leftJoin(area, eq(area.id, sector.areaId))
+  .leftJoin(
+    climbingLevelNoRope,
+    or(
+      eq(climbingLevelNoRope.vScale, grade.value),
+      eq(climbingLevelNoRope.fontainebleau, grade.value)
+    )
+  )
+  .where(
+    and(
+      eq(grade.userId, userId),
+      eq(grade.accomplished, true),
+      inArray(grade.gradeSystem, ['VScale', 'Fontainebleau'])
+    )
+  )
+  .orderBy(desc(climbingLevelNoRope.scaledValue))
+  .limit(3);
+const topTrad = await db
+  .select({
+    areaId: area.id,
+    areaName: area.name,
+    sectorName: sector.name,
+    climbName: climb.name,
+    gradeValue: grade.value,
+    scaledValue: climbingLevelTrad.scaledValue,
+    createdAt: grade.createdAt
+  })
+  .from(grade)
+  .leftJoin(climb, eq(climb.id, grade.climbId))
+  .leftJoin(sector, eq(sector.id, climb.sectorId))
+  .leftJoin(area, eq(area.id, sector.areaId))
+  .leftJoin(
+    climbingLevelTrad,
+    eq(climbingLevelTrad.british, grade.value)
+  )
+  .where(
+    and(
+      eq(grade.userId, userId),
+      eq(grade.accomplished, true),
+      eq(grade.gradeSystem, 'British')
+    )
+  )
+  .orderBy(desc(climbingLevelTrad.scaledValue))
+  .limit(3);
+
+
 
 	return {
 		items,
 		page,
-		status
+		status,
+		userRopeClimbs,
+		userTradClimbs,
+		userNoRopeClimbs,
 	};
 };
